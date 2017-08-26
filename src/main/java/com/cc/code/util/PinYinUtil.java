60923e564c7f2ee210db1c95790edda3922d6970
cc <cong@congdeMBP.lan>
2017-08-26 09:10:21 +0800

    no message

--- src/main/java/com/cc/code/util/PinYinUtil.java
+++ src/main/java/com/cc/code/util/PinYinUtil.java
@@ -7,67 +7,43 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 /**
- * 
  * @author jlon
- *
  */
 public class PinYinUtil {
-	/**
-	 * 获取汉字串拼音首字母，英文字符不变
-	 * 
-	 * @param chinese
-	 *            汉字串
-	 * @return 汉语拼音首字母
-	 */
-	public static String getFirstSpell(String chinese) {
-		StringBuffer pybf = new StringBuffer();
-		char[] arr = chinese.toCharArray();
-		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > 128) {
-				try {
-					String[] temp = PinyinHelper.toHanyuPinyinStringArray(
-							arr[i], defaultFormat);
-					if (temp != null) {
-						pybf.append(temp[0].charAt(0));
-					}
-				} catch (BadHanyuPinyinOutputFormatCombination e) {
-					e.printStackTrace();
-				}
-			} else {
-				pybf.append(arr[i]);
-			}
-		}
-		return pybf.toString().replaceAll("\\W", "").trim();
-	}
+    /**
+     * 获取汉字串拼音首字母，英文字符不变
+     *
+     * @param chinese 汉字串
+     * @return 汉语拼音首字母
+     */
+    public static String getFirstSpell(String chinese) {
+        return chinese;
+    }
 
-	/**
-	 * 获取汉字串拼音，英文字符不变
-	 * 
-	 * @param chinese
-	 *            汉字串
-	 * @return 汉语拼音
-	 */
-	public static String getFullSpell(String chinese) {
-		StringBuffer pybf = new StringBuffer();
-		char[] arr = chinese.toCharArray();
-		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > 128) {
-				try {
-					pybf.append(PinyinHelper.toHanyuPinyinStringArray(arr[i],
-							defaultFormat)[0]);
-				} catch (BadHanyuPinyinOutputFormatCombination e) {
-					e.printStackTrace();
-				}
-			} else {
-				pybf.append(arr[i]);
-			}
-		}
-		return pybf.toString();
-	}
+    /**
+     * 获取汉字串拼音，英文字符不变
+     *
+     * @param chinese 汉字串
+     * @return 汉语拼音
+     */
+    public static String getFullSpell(String chinese) {
+        StringBuffer pybf = new StringBuffer();
+        char[] arr = chinese.toCharArray();
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 128) {
+                try {
+                    pybf.append(PinyinHelper.toHanyuPinyinStringArray(arr[i],
+                            defaultFormat)[0]);
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            } else {
+                pybf.append(arr[i]);
+            }
+        }
+        return pybf.toString();
+    }
 }
